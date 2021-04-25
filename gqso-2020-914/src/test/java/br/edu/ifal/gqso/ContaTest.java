04be9c494229ee8ced1e5705cca9a4c5721fd10c
SamelaTenorio <chimmineykookie@gmail.com>
2021-04-25 14:12:04 +0000

    teste getSaldo

--- gqso-2020-914/src/test/java/br/edu/ifal/gqso/ContaTest.java
+++ gqso-2020-914/src/test/java/br/edu/ifal/gqso/ContaTest.java
@@ -1,5 +1,7 @@
 package br.edu.ifal.gqso;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class ContaTest {
@@ -7,6 +9,6 @@ public class ContaTest {
     @Test
     public void contaTest(){
         Conta conta = new Conta("Sâmela Tenório", "000000000-00",123, 0.0);   
-        
+        assertEquals(0.0, conta.getSaldo());
     }
 }
